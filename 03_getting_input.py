d01ef8d4d02c58e1c997ca37e8821a8989890375
jacksona1143 <101374259+jacksona1143@users.noreply.github.com>
2022-03-23 10:48:15 +1300

    Create 03_getting_input.py

---
+++ 03_getting_input.py
@@ -0,0 +1,16 @@
+
+username = input("What is your name? ")
+
+fav_num = int(input("Favourite Number? "))
+
+double = fav_num * 2
+
+halve = fav_num / 2
+
+squared = fav_num * fav_num
+
+
+print(" Hi",username, "your favourite number is", fav_num)
+print(" Hi",username, "your favourite number doubled is", double)
+print(" Hi",username, "your favourite number halved is", halve)
+print(" Hi",username, "your favourite number squared is", squared)
